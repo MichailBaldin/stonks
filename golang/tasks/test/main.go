c5a9cfe86f948f97f31541e19939935dcc869d81
baldin <michailbaldin@gmail.com>
2025-07-22 16:52:44 +0300

    some changes

--- golang/tasks/test/main.go
+++ golang/tasks/test/main.go
@@ -1,22 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	resp, err := http.Get("https://api.github.com/users/octocat")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(body))
+	a := "A"
+	b := "B"
+	fmt.Println(a + b)
 }
